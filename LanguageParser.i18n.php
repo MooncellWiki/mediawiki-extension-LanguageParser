99dc329437775a08a14f31a944f49c2182e85382
StarHeartHunt <starheart233@gmail.com>
2020-06-06 21:29:51 +0800

    init

---
+++ LanguageParser.i18n.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @license GPL-2.0-or-later
+ * @author Your Name (YourUserName)
+ */
+
+$magicWords = [];
+
+/** English
+ * @author Your Name (YourUserName)
+ */
+$magicWords['en'] = [
+   'tsl' => [ 0, 'tsl' ],
+];
+
+/** English
+ * @author Your Name (YourUserName)
+ */
+$magicWords['zh'] = [
+   'tsl' => [ 0, 'tsl' ],
+];
+
+/** English
+ * @author Your Name (YourUserName)
+ */
+$magicWords['zh-cn'] = [
+   'tsl' => [ 0, 'tsl' ],
+];
---
+++ README.md
@@ -0,0 +1,4 @@
+# LanguageParser
+An Extension to Solve i18n Problem.
+
+Examples are in [PRTS Wiki](http://ak.mooncell.wiki/)
